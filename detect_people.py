6b87f2035efae30b2385d0aa14f52f98b6198bad
glinkowski <linkowski@hotmail.com>
2016-03-23 20:48:25 -0500

    first pass at people detection
    Rudimentary people detector using Histogram of Oriented Gradients

---
+++ detect_people.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+import sys
+from imutils.object_detection import non_max_suppression
+
+video = cv2.VideoCapture(sys.argv[1])
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+
+out = cv2.VideoWriter('output.avi', cv2.cv.CV_FOURCC('M','J','P','G'), 30.0, (640,480), True)
+
+
+
+while video.isOpened():
+	ret, frame = video.read()
+	frame = cv2.resize(frame, (640,480))
+	if ret:
+		(rects, weights) = hog.detectMultiScale(frame, winStride=(4,4), padding=(8,8), scale=1.05)
+
+		rects = np.array([[x,y,x+w,y+h] for (x,y,w,h) in rects])
+		pick = non_max_suppression(rects, probs = None, overlapThresh = 0.65)
+
+		for (xA, yA, xB, yB) in pick:
+			cv2.rectangle(frame, (xA,yA), (xB,yB), (0,255,0), 2)
+
+		
+
+		out.write(frame)
+
+		print "processed"
+
+video.release()
+out.release()
+
