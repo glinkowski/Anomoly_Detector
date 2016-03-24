0c56a86ad4fde8e0fe51835bd9295690bd60f595
glinkowski <linkowski@hotmail.com>
2016-03-23 21:39:36 -0500

    added converstion to grayscale

--- getclip.py
+++ getclip.py
@@ -1,15 +1,40 @@
+# ---------------------------------------------------------
+# author: Greg Linkowski
+# project: Anomoly Detection
+#		for Computer Vision final project
+# 
+# Crop & Downsample the video
+#	Downsample both the resoultion and the frames
+# 
+# ---------------------------------------------------------
+
 import cv2
 import sys
 
 
+
+####### ####### ####### ####### 
+# PARAMETERS
+
 # number of frames to skip / downsample
 nSkip = 0
+
 # total number of frames for final video
-nFinal =250
+nFinal = 250
+
+convertToBW = True
+
+####### ####### ####### ####### 
+
+
+
+####### ####### ####### ####### 
+# BEGIN MAIN FUNCTION
 
 
 inVid = cv2.VideoCapture(sys.argv[1])
-outVid = cv2.VideoWriter('clip.avi', cv2.cv.CV_FOURCC('M','J','P','G'), 30.0, (640,480), True)
+
+outVid = cv2.VideoWriter('clip.avi', cv2.cv.CV_FOURCC('M','J','P','G'), 30.0, (640,480), (not convertToBW))
 
 
 count = 0
@@ -35,11 +60,17 @@ while inVid.isOpened():
 		# Resize to smaller scale
 		frame = cv2.resize(frame, (640,480))
 
+		# Convert to grayscale
+		if convertToBW :
+			frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+		#end if
+
 		outVid.write(frame)
 		added += 1
 
 		if not (added % 25) :
 			print("processed {}".format(added))
+		#end if
 	else :
 		break
 	#end if
@@ -50,3 +81,4 @@ inVid.release()
 outVid.release()
 
 
+print("\nDone.\n")
