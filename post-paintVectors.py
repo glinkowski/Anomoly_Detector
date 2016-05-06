b61ba9c015f7c6a415fcba4d93a6f1cdc9e8fde0
glinkowski <linkowski@hotmail.com>
2016-05-06 00:14:10 -0500

    fixed drawing of the vectors
    Had made an assumption about the values of the vector
    endpoints. Now just using the origin and calculating
    endpoints from magnitude and angle.

--- post-paintVectors.py
+++ post-paintVectors.py
@@ -17,6 +17,7 @@
 import cv2
 import sys
 import numpy as np
+import math
 
 
 
@@ -26,8 +27,13 @@ import numpy as np
 inVidName = 'mntl01.MTS'
 inVidDir = 'MNTL/'
 
-mvSuffix = '-vectors.txt'
+#drawColor = (128, 255, 64)
+#drawColor = (180, 20, 200)
+#drawColor = (255, 0, 255)
+drawColor = (0, 192, 255)
 
+
+mvSuffix = '-vectors.txt'
 inVidFile = inVidDir + inVidName
 mvFile = inVidDir + inVidName.split('.')[0] + mvSuffix
 
@@ -58,10 +64,24 @@ with open(mvFile, 'r') as fin :
 #		print lv
 		if (float(lv[2]) > 200) :	continue
 		cv2.circle(stillFrame, (int(lv[0]), int(lv[1])),
-			6, (64,192,0), 6)
+			6, drawColor, 6)
+		x2 = int(lv[0]) + float(lv[2])*math.cos(float(lv[3]))
+		x2 = int(round(x2))
+		y2 = int(lv[1]) + float(lv[2])*math.sin(float(lv[3]))
+		y2 = int(round(y2))
+#		print x2, y2
 		cv2.line(stillFrame, (int(lv[0]), int(lv[1])),
-			(int(lv[4]), int(lv[5])), (64,192,0), 4)
+			(x2, y2), drawColor, 4)
+
+		# cv2.line(stillFrame, (int(lv[0]), int(lv[1])),
+		# 	(int(lv[4]), int(lv[5])), (64,192,0), 4)
 #end with
+
+
+# Display image (buggy)
+# cv2.imshow("still", stillFrame);
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 
 # Output the image
